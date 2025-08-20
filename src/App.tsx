1e3ecd839b210b23f949cb2b0da344ddfe7dc465
LeandroFSimeao <leandrosimeao@yahoo.com.br>
2025-08-20 00:10:12 -0300

    Add lucide-react dependency and refactor components
    - Introduced lucide-react for icons in the Logo and Menu components.
    - Replaced Heading component with Container, Logo, Menu, and CountDown components.
    - Updated global and theme styles for better design consistency.
    - Removed unused Heading component and its styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  console.log('Oi');
-
   return (
     <>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
-        eum, odit maxime iure tempora vel officiis aliquid, quisquam quas amet
-        vero exercitationem adipisci expedita molestiae voluptate optio
-        nesciunt. Quidem, atque.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
     </>
   );
 }
